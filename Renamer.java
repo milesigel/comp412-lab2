dfd60cc4b3df2cb36e5314655de45c83815bfc92
Miles Sigel <mgs6@rice.edu>
2022-09-29 17:10:32 -0500

    small changes to renamer

--- Renamer.java
+++ Renamer.java
@@ -3,6 +3,21 @@ import intermediate_representation.IRNode;
 
 public class Renamer {
 
+    /**
+     *         tokenConversion[0] = "MEMOP";
+     *         tokenConversion[1] = "LOADI";
+     *         tokenConversion[2] = "ARITHOP";
+     *         tokenConversion[3] = "OUTPUT";
+     *         tokenConversion[4] = "NOP";
+     *         tokenConversion[5] = "CONSTANT";
+     *         tokenConversion[6] = "REGISTER";
+     *         tokenConversion[7] = "COMMA";
+     *         tokenConversion[8] = "INTO";
+     *         tokenConversion[9] = "EOF";
+     *         tokenConversion[10] = "EOL";
+     *         tokenConversion[11] = "REG";
+     */
+
     private IRList representation;
     private int vrName = 0;
     private int maxSR;
@@ -20,16 +35,29 @@ public class Renamer {
         this.representation = representation;
         this.maxSR = maxSR;
         this.SRtoVR = new int[this.maxSR];
-        this.LU = new int[this.maxSR];
-
+        this.LU = new int[this.maxSR + 1];
+        for (int i = 0; i < maxSR + 1; i++) {
+            this.SRtoVR[i] = -1;
+            this.LU[i] = -1;
+        }
     }
 
     public void addVirtualRegisters() {
-        IRNode workingNode = representation.getTail();
+        IRNode workingNode = representation.getTail().getPrev();
         IRNode head = representation.getHead();
+        int source;
+        int opCode;
         while (workingNode != head) {
+            opCode = workingNode.getOpCode();
+            switch (opCode) {
+                case 0 : {
 
+                    break;
+                }
+                default : {
 
+                }
+            }
         }
     }
 }
